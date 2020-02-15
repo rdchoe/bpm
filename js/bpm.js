9345ffbcfab18d0fc4c678417bdc58e59e6314f3
Robert Choe <robertchoe@wireless-10-105-87-92.umd.edu>
2020-02-15 04:06:37 -0500

    updates bpm on front end

--- js/bpm.js
+++ js/bpm.js
@@ -13,11 +13,11 @@ function printBpm() {
 // audioCtx.resume()
 $('body').on('keyup', function() {
 	if (!audioCtx) {
-		console.log("initialziing audio context")
+		// console.log("initialziing audio context")
 		initAudio();
 	}
 	if (audioCtx.state === 'suspended'){
-		console.log("resuming")
+		// console.log("resuming")
 		audioCtx.resume();
 	}
 	clearInterval(interval)
@@ -26,9 +26,10 @@ $('body').on('keyup', function() {
 	prev_timestamp = curr_timestamp
 	let bpm = 60000/difference
 	console.log(bpm)
-	curr_bpm = bpm
+	curr_bpm = parseInt(bpm, 10)
 	let beats_per_ms = 60000/bpm
   interval = setInterval(printBpm,beats_per_ms)
+	$('#bpm').html(curr_bpm)
 })
 
 function initAudio() {
